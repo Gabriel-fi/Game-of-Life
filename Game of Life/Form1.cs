c59a583ef5af1a27fcbd7e544a9b7bdb82e5a2a5
Gabriel Figueiredo B. Leo <gabrielboneragp@hotmail.com>
2016-05-05 22:56:41 -0400

    Re-Wrote all functionality of cells

--- Game of Life/Form1.cs
+++ Game of Life/Form1.cs
@@ -12,7 +12,7 @@ namespace Game_of_Life
 {
     public partial class Form1 : Form
     {
-        bool[,] universe = new bool[40, 40];
+        bool[,] universe = new bool[20, 20];
         bool[,] spad = new bool[40, 40];
         Timer timer = new Timer();
         int gens = 0;
@@ -22,7 +22,9 @@ namespace Game_of_Life
         public Form1()
         {
             InitializeComponent();
-            timer.Interval = 20;
+            timer.Interval = 70;
+            timer.Tick += Timer_Tick;
+            timer.Stop();
         }
 
         private void Timer_Tick(object sender, EventArgs e)
@@ -31,6 +33,7 @@ namespace Game_of_Life
             gens++;
             //Update status
             GenerationStatus.Text = "Generations: " + gens.ToString();
+            Update2();
             graphicsPanel1.Invalidate();
         }
 
@@ -55,7 +58,7 @@ namespace Game_of_Life
 
                     if (universe[(int)x, (int)y] == true)
                     {
-                        e.Graphics.FillRectangle(Brushes.Black, rect.X, rect.Y, rect.Width, rect.Height);
+                        e.Graphics.FillRectangle(Brushes.DodgerBlue, rect.X, rect.Y, rect.Width, rect.Height);
                     }
 
                     e.Graphics.DrawRectangle(Pens.Black, rect.X, rect.Y, rect.Width, rect.Height);
@@ -63,74 +66,155 @@ namespace Game_of_Life
             }
         }
 
+        private static int AdjChecks(bool[,] universe, int ScreenSize, int x, int y, int horizontal, int veritcal)
+        {
+            int amt = 0;
+
+            int boundsX = x + horizontal;
+            int boundsY = y + veritcal;
+            bool outOfBounds = boundsX < 0 || boundsX >= ScreenSize || boundsY < 0
+                || boundsY >= ScreenSize;
+            if (!outOfBounds)
+            {
+                amt = universe[x + horizontal, y + veritcal] ? 1 : 0;
+            }
+            return amt;
+        }
+        private void Update2()
+        {
+            int sizeX = universe.GetLength(0);
+            int sizeY = universe.GetLength(1);
+            int size = 20;
+
+            for (int i = 0; i < sizeX; i++)
+            {
+                for (int t = 0; t < sizeY; t++)
+                {
+                    spad[i, t] = universe[i, t];
+                }
+            }
+            for (int x = 0; x < sizeX; x++)
+            {
+                for (int y = 0; y < sizeY; y++)
+                {
+                    int count = AdjChecks(universe, size, x, y, -1, 0) + AdjChecks(universe, size, x, y, -1, 1) 
+                        + AdjChecks(universe, size, x, y, 0, 1) + AdjChecks(universe, size, x, y, 1, 1) + AdjChecks(universe, size, x, y, 1, 0) 
+                        + AdjChecks(universe, size, x, y, 1, -1) + AdjChecks(universe, size, x, y, 0, -1) + AdjChecks(universe, size, x, y, -1, -1);
+                    bool isAlive = universe[x, y];
+                    bool TurnOn = false;
+
+                    if (isAlive && (count == 2 || count == 3))
+                    {
+                        TurnOn = true;
+                    }
+                    else if (!isAlive && count == 3)
+                    {
+                        TurnOn = true;
+                    }
+
+                    spad[x, y] = TurnOn;
+                }
+            }
+            for (int i = 0; i < size; i++)
+            {
+                for (int t = 0; t < size; t++)
+                {
+                    universe[i, t] = spad[i, t];
+                }
+            }
+        }
+        /*
         private void Update1()
         {
+            int sizeX = universe.GetLength(0);
+            int sizeY = universe.GetLength(1);
+            int count = 0;
+            for (int a = 0; a < sizeX; a++)
+            {
+                for (int i = 0; i < sizeY; i++)
+                {
+                    spad[a, i] = universe[a, i];
+                    
+                }
+            }
             for (int x = 0; x < universe.GetLength(0); x++) //X-Axis
             {
                 for (int y = 0; y < universe.GetLength(1); y++) //Y-Axis
                 {
-                    if (universe[x, y] == true)
-                    {
-                        int count = 0;
-                        //Check top
-                        if (universe[x, y - 1] == true)
-                            count++;
-                        //Check top-right
-                        if (universe[x + 1, y - 1] == true)
-                            count++;
-                        //Check right
-                        if (universe[x + 1, y] == true)
-                            count++;
-                        //Check bottom-right
-                        if (universe[x + 1, y + 1] == true)
-                            count++;
-                        //Check-bottom
-                        if (universe[x, y + 1] == true)
-                            count++;
-                        //Check bottom-left
-                        if (universe[x - 1, y + 1] == true)
-                            count++;
-                        //Check left
-                        if (universe[x - 1, y] == true)
-                            count++;
-                        //Check top-left
-                        if (universe[x - 1, y - 1] == true)
-                            count++;
+                    //Check top
+                    if (y - 1 >= 0 && universe[x, y - 1] == true)
+                        count++;
+                    //Check top-right
+                    if (y - 1 >= 0 && x + 1 < sizeX && universe[x + 1, y - 1] == true)
+                        count++;
+                    //Check right
+                    if (x + 1 < sizeX && universe[x + 1, y] == true)
+                        count++;
+                    //Check bottom-right
+                    if (y + 1 < sizeY && x + 1 < sizeX && universe[x + 1, y + 1] == true)
+                        count++;
+                    //Check-bottom
+                    if (y + 1 < sizeY && universe[x, y + 1] == true)
+                        count++;
+                    //Check bottom-left
+                    if (y + 1 < sizeY && x - 1 >= 0 && universe[x - 1, y + 1] == true)
+                        count++;
+                    //Check left
+                    if (x-1>=0 && universe[x - 1, y] == true)
+                        count++;
+                    //Check top-left
+                    if (y - 1 >= 0 && x - 1 >= 0 && universe[x - 1, y - 1] == true)
+                        count++;
 
-                        //Living is dying
-                        if (count > 3)
-                            spad[x, y] = false;
-                        //Living in the next generation
-                        if (count == 2 || count == 3)
-                            spad[x, y] = true;
-                        //Living is dying
-                        if (count == 1 || count == 0)
-                            spad[x, y] = false;
-                        //Dead will Live
-                        if (count == 3)
-                            spad[x, y] = true;
-                    }
+                    ////////////////Living is dying
+                    //if (count > 3)
+                    //spad[x, y] = false;
+                    //////////////////Living in the next generation
+                    if (universe[x, y] && (count == 2 || count == 3))
+                        spad[x, y] = true;
+                    ///////////////////Living is dying
+                    //else if (count <= 1)
+                    //spad[x, y] = false;
+                    //////////////Dead will Live
+                    else if (universe[x, y] && count == 3)
+                        spad[x, y] = true;
                 }
             }
-            universe = spad;
-            spad = new bool[40, 40];
-        }
+            //bool[,] tmp = universe;
+            //universe = spad;
+            //spad = tmp;
 
+            
+            for (int x = 0; x < universe.GetLength(0); x++) //X-Axis
+            {
+                for (int y = 0; y < universe.GetLength(1); y++) //Y-Axis
+                {
+                    universe[x, y] = spad[x, y];
+                    //spad[x, y] = false;
+                }
+            }
+            spad = new bool[40, 40];
+            
+        }
+        */
         private void graphicsPanel1_MouseClick(object sender, MouseEventArgs e)
         {
             float w = graphicsPanel1.ClientSize.Width / universe.GetLength(0);
             float h = graphicsPanel1.ClientSize.Height / universe.GetLength(1);
-
-            if (e.Button == MouseButtons.Left)
+            try
             {
-                float x = e.X / w;
-                float y = e.Y / h;
+                if (e.Button == MouseButtons.Left)
+                {
+                    float x = e.X / w;
+                    float y = e.Y / h;
 
 
-                universe[(int)x, (int)y] = !universe[(int)x, (int)y];
-
+                    universe[(int)x, (int)y] = !universe[(int)x, (int)y];
+                }
                 graphicsPanel1.Invalidate();
             }
+            catch (IndexOutOfRangeException) { }
+
         }
 
         private void newToolStripMenuItem1_Click(object sender, EventArgs e)
@@ -142,14 +226,15 @@ namespace Game_of_Life
                     universe[x, y] = false;
                 }
             }
+            timer.Stop();
+            gens = 0;
+            GenerationStatus.Text = "Generations: " + gens.ToString();
             graphicsPanel1.Invalidate();
         }
 
         private void PlayButton_Click(object sender, EventArgs e)
         {
             timer.Start();
-            timer.Tick += Timer_Tick;
-            Update1();
         }
 
         private void PauseButton_Click(object sender, EventArgs e)
@@ -159,8 +244,17 @@ namespace Game_of_Life
 
         private void NextButton_Click(object sender, EventArgs e)
         {
-            timer.Tick += Timer_Tick;
-            Update1();
+            //Calculate the new Generation
+            gens++;
+            //Update status
+            GenerationStatus.Text = "Generations: " + gens.ToString();
+            Update2();
+            graphicsPanel1.Invalidate();
+        }
+
+        private void newToolStripButton_Click(object sender, EventArgs e)
+        {
+            newToolStripMenuItem1_Click(sender, e);
         }
     }
 }
